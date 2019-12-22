ed8e896662309d638fa8ce372fc61095fc25336b
paladinze <paladinze@hotmail.com>
2019-12-22 11:29:53 +0800

    added operators

--- operator.js
+++ operator.js
@@ -1,8 +1,13 @@
-const {
-  Observable
-} = require('rxjs');
-const { 
-  share,
-  shareReplay,
-  publish
-} = require('rxjs/operators');
+const { Observable, from } = require("rxjs");
+const { map, filter, take, scan, tap } = require("rxjs/operators");
+
+// combine operators through pipe
+const sourceOb = from([1, 2, 3, 4, 5])
+const modifiedOb = sourceOb.pipe(
+  tap(n => console.log(`tapped: ${n}`)),
+  filter(n => n < 5),
+  map(n => n * 2),
+  scan((acc, val) => acc + val ), // same as reduce
+  take(3) // take 3 and complete
+)
+modifiedOb.subscribe(console.log)
